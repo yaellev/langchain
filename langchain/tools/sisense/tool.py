06fb84af309bd2cd0d92e3aafca681a5774a9635
Yael Lev <yaellev@users.noreply.github.com>
2023-06-22 18:56:51 -0400

    Update tool.py

--- langchain/tools/sisense/tool.py
+++ langchain/tools/sisense/tool.py
@@ -1,4 +1,4 @@
-"""Tools for interacting with a Power BI dataset."""
+"""Tools for interacting with a Sisense dataset."""
 import logging
 from typing import Any, Dict, Optional, Tuple
 
@@ -10,28 +10,28 @@ from langchain.callbacks.manager import (
 )
 from langchain.chains.llm import LLMChain
 from langchain.tools.base import BaseTool
-from langchain.tools.powerbi.prompt import (
+from langchain.tools.Sisense.prompt import (
     BAD_REQUEST_RESPONSE,
     DEFAULT_FEWSHOT_EXAMPLES,
     QUESTION_TO_QUERY,
     RETRY_RESPONSE,
 )
-from langchain.utilities.powerbi import PowerBIDataset, json_to_md
+from langchain.utilities.Sisense import SisenseDataset, json_to_md
 
 logger = logging.getLogger(__name__)
 
 
-class QueryPowerBITool(BaseTool):
-    """Tool for querying a Power BI Dataset."""
+class QuerySisenseTool(BaseTool):
+    """Tool for querying a Sisense Dataset."""
 
-    name = "query_powerbi"
+    name = "query_Sisense"
     description = """
     Input to this tool is a detailed question about the dataset, output is a result from the dataset. It will try to answer the question using the dataset, and if it cannot, it will ask for clarification.
 
     Example Input: "How many rows are in table1?"
     """  # noqa: E501
     llm_chain: LLMChain
-    powerbi: PowerBIDataset = Field(exclude=True)
+    Sisense: SisenseDataset = Field(exclude=True)
     template: Optional[str] = QUESTION_TO_QUERY
     examples: Optional[str] = DEFAULT_FEWSHOT_EXAMPLES
     session_cache: Dict[str, Any] = Field(default_factory=dict, exclude=True)
@@ -54,7 +54,7 @@ class QueryPowerBITool(BaseTool):
             "examples",
         ]:
             raise ValueError(
-                "LLM chain for QueryPowerBITool must have input variables ['tool_input', 'tables', 'schemas', 'examples'], found %s",  # noqa: C0301 E501 # pylint: disable=C0301
+                "LLM chain for QuerySisenseTool must have input variables ['tool_input', 'tables', 'schemas', 'examples'], found %s",  # noqa: C0301 E501 # pylint: disable=C0301
                 llm_chain.prompt.input_variables,
             )
         return llm_chain
@@ -80,11 +80,11 @@ class QueryPowerBITool(BaseTool):
             return cache
 
         try:
-            logger.info("Running PBI Query Tool with input: %s", tool_input)
+            logger.info("Running sisense Query Tool with input: %s", tool_input)
             query = self.llm_chain.predict(
                 tool_input=tool_input,
-                tables=self.powerbi.get_table_names(),
-                schemas=self.powerbi.get_schemas(),
+                tables=self.Sisense.get_table_names(),
+                schemas=self.Sisense.get_schemas(),
                 examples=self.examples,
             )
         except Exception as exc:  # pylint: disable=broad-except
@@ -94,8 +94,8 @@ class QueryPowerBITool(BaseTool):
             self.session_cache[tool_input] = query
             return self.session_cache[tool_input]
         logger.info("Query: %s", query)
-        pbi_result = self.powerbi.run(command=query)
-        result, error = self._parse_output(pbi_result)
+        sisense_result = self.Sisense.run(command=query)
+        result, error = self._parse_output(sisense_result)
 
         iterations = kwargs.get("iterations", 0)
         if error and iterations < self.max_iterations:
@@ -123,11 +123,11 @@ class QueryPowerBITool(BaseTool):
             logger.debug("Found cached result for %s: %s", tool_input, cache)
             return cache
         try:
-            logger.info("Running PBI Query Tool with input: %s", tool_input)
+            logger.info("Running sisense Query Tool with input: %s", tool_input)
             query = await self.llm_chain.apredict(
                 tool_input=tool_input,
-                tables=self.powerbi.get_table_names(),
-                schemas=self.powerbi.get_schemas(),
+                tables=self.Sisense.get_table_names(),
+                schemas=self.Sisense.get_schemas(),
                 examples=self.examples,
             )
         except Exception as exc:  # pylint: disable=broad-except
@@ -138,8 +138,8 @@ class QueryPowerBITool(BaseTool):
             self.session_cache[tool_input] = query
             return self.session_cache[tool_input]
         logger.info("Query: %s", query)
-        pbi_result = await self.powerbi.arun(command=query)
-        result, error = self._parse_output(pbi_result)
+        sisense_result = await self.Sisense.arun(command=query)
+        result, error = self._parse_output(sisense_result)
 
         iterations = kwargs.get("iterations", 0)
         if error and iterations < self.max_iterations:
@@ -157,33 +157,33 @@ class QueryPowerBITool(BaseTool):
         return self.session_cache[tool_input]
 
     def _parse_output(
-        self, pbi_result: Dict[str, Any]
+        self, sisense_result: Dict[str, Any]
     ) -> Tuple[Optional[str], Optional[str]]:
         """Parse the output of the query to a markdown table."""
-        if "results" in pbi_result:
-            return json_to_md(pbi_result["results"][0]["tables"][0]["rows"]), None
+        if "results" in sisense_result:
+            return json_to_md(sisense_result["results"][0]["tables"][0]["rows"]), None
 
-        if "error" in pbi_result:
+        if "error" in sisense_result:
             if (
-                "pbi.error" in pbi_result["error"]
-                and "details" in pbi_result["error"]["pbi.error"]
+                "sisense.error" in sisense_result["error"]
+                and "details" in sisense_result["error"]["sisense.error"]
             ):
-                return None, pbi_result["error"]["pbi.error"]["details"][0]["detail"]
-            return None, pbi_result["error"]
+                return None, sisense_result["error"]["sisense.error"]["details"][0]["detail"]
+            return None, sisense_result["error"]
         return None, "Unknown error"
 
 
-class InfoPowerBITool(BaseTool):
-    """Tool for getting metadata about a PowerBI Dataset."""
+class InfoSisenseTool(BaseTool):
+    """Tool for getting metadata about a Sisense Dataset."""
 
-    name = "schema_powerbi"
+    name = "schema_Sisense"
     description = """
     Input to this tool is a comma-separated list of tables, output is the schema and sample rows for those tables.
-    Be sure that the tables actually exist by calling list_tables_powerbi first!
+    Be sure that the tables actually exist by calling list_tables_Sisense first!
 
     Example Input: "table1, table2, table3"
     """  # noqa: E501
-    powerbi: PowerBIDataset = Field(exclude=True)
+    Sisense: SisenseDataset = Field(exclude=True)
 
     class Config:
         """Configuration for this pydantic object."""
@@ -196,22 +196,22 @@ class InfoPowerBITool(BaseTool):
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         """Get the schema for tables in a comma-separated list."""
-        return self.powerbi.get_table_info(tool_input.split(", "))
+        return self.Sisense.get_table_info(tool_input.split(", "))
 
     async def _arun(
         self,
         tool_input: str,
         run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
     ) -> str:
-        return await self.powerbi.aget_table_info(tool_input.split(", "))
+        return await self.Sisense.aget_table_info(tool_input.split(", "))
 
 
-class ListPowerBITool(BaseTool):
+class ListSisenseTool(BaseTool):
     """Tool for getting tables names."""
 
-    name = "list_tables_powerbi"
+    name = "list_tables_Sisense"
     description = "Input is an empty string, output is a comma separated list of tables in the database."  # noqa: E501 # pylint: disable=C0301
-    powerbi: PowerBIDataset = Field(exclude=True)
+    Sisense: SisenseDataset = Field(exclude=True)
 
     class Config:
         """Configuration for this pydantic object."""
@@ -224,7 +224,7 @@ class ListPowerBITool(BaseTool):
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         """Get the names of the tables."""
-        return ", ".join(self.powerbi.get_table_names())
+        return ", ".join(self.Sisense.get_table_names())
 
     async def _arun(
         self,
@@ -232,4 +232,4 @@ class ListPowerBITool(BaseTool):
         run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
     ) -> str:
         """Get the names of the tables."""
-        return ", ".join(self.powerbi.get_table_names())
+        return ", ".join(self.Sisense.get_table_names())
